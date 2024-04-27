f525314585c503375bdc6753ff01eeeaee8dcc21
izabela <limasouzaizabela6@gmail.com>
2024-04-26 22:34:58 -0300

    Criação do complemento do cep

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,23 @@
 function consultacep()
 {
+    let campo= document.querySelector("#cep");
     let requisicao = new XMLHttpRequest();
-    let url = "https://viacep.com.br/ws/17015220/json/";
+    let url = "https://viacep.com.br/ws/"+campo.value+"/json/";
     requisicao.open("GET" , url);
     requisicao.send();
     requisicao.onload = function(){
-        alert(JSON.parse(requisicao.response).logradouro);
+        let objetoCEP = JSON.parse(requisicao.response)
+        let CampoEndereço = document.querySelector("#logradouro")
+        let Campocomplemento = document.querySelector("#complemento")
+        let CampoBairro = document.querySelector("#Bairro")
+        let Campolocalidade = document.querySelector("#localidade")
+        alert(objetoCEP.uf + " - "+ objetoCEP.localidade)
+
+        CampoEndereço.value = objetoCEP.logradouro
+        CampoBairro.value = objetoCEP.bairro
+        Campocomplemento.value = objetoCEP.complemento
+        Campolocalidade.value = objetoCEP.localidade
+
     }
 }
 let botao = document.querySelector("#botao");
